7e093a4c90a5f2c499738334900644eafaee4890
AnasFarhane <76697224+AnasFarhane@users.noreply.github.com>
2022-12-11 12:51:29 +0100

    1rst

--- src/main/java/com/example/jenkinsdemo/WelcomeController.java
+++ src/main/java/com/example/jenkinsdemo/WelcomeController.java
@@ -1,12 +1,12 @@
 package com.example.jenkinsdemo;
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class WelcomeController {
 
-    @RequestMapping("/welcome")
+    @GetMapping("/welcome")
     public String welcome(){
         return "Hey, I am from external tomcat";
     }
